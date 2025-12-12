1f8e8b8692731496bbf53398defc50a22ef1f3c8
trebla915 <tr3bla915@gmail.com>
2025-12-11 17:00:15 -0700

    Style: change header border from gray to brand yellow

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -26,7 +26,7 @@ const Header = () => {
   };
 
   return (
-    <header className="sticky top-0 z-50 bg-brand-black border-b border-gray-800">
+    <header className="sticky top-0 z-50 bg-brand-black border-b border-brand-yellow">
       <nav className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-24">
           {/* Logo */}
